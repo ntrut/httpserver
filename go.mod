a4e8b5ac7c85d450f3e3a2b8c072beed4f722e62
ntrut <nazartrut@gmail.com>
2021-10-20 12:31:14 -0700

    httpserver.go      -added another endpoint GET /all      -returns all items from Crypto dynamoDB

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.13
 
 require (
 	github.com/TwinProduction/go-color v1.0.0 // indirect
+	github.com/aws/aws-sdk-go v1.41.6
 	github.com/gorilla/mux v1.8.0
 	github.com/jamespearly/loggly v0.0.0-20210916012910-7f219cd27651
+	github.com/joho/godotenv v1.4.0
 )
--- httpserver.go
+++ httpserver.go
@@ -2,14 +2,46 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
+	//"github.com/aws/aws-sdk-go/aws"
+	//"github.com/aws/aws-sdk-go/aws/session"
+	//"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/gorilla/mux"
 	"github.com/jamespearly/loggly"
+	"github.com/joho/godotenv"
 )
+
+type information struct {
+	Count        int64 `json:"Count"`
+	Items        Item  `json:"Items"`
+	ScannedCount int64 `json:"ScannedCount"`
+}
+
+type Item struct {
+	Timestamp         int64  `json:"timestamp"`
+	Id                string `json:"id"`
+	Rank              string `json:"rank"`
+	Symbol            string `json:"symbol"`
+	Name              string `json:"name"`
+	Supply            string `json:"supply"`
+	MaxSupply         string `json:"maxSupply"`
+	MarketCapUsd      string `json:"marketCapUsd"`
+	VolumeUsd24Hr     string `json:"volumeUsd24Hr"`
+	PriceUsd          string `json:"priceUsd"`
+	ChangePercent24hr string `json:"changePercent24hr"`
+	Vwap24Hr          string `json:"vwap24Hr"`
+	Explorer          string `json:"explorer"`
+}
 
 func statusHandler(w http.ResponseWriter, r *http.Request) {
 
@@ -60,10 +92,69 @@ func statusHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, string(output))
 }
 
+func all(w http.ResponseWriter, r *http.Request) {
+
+	err := godotenv.Load("app.env")
+	if err != nil {
+		fmt.Println("Error loading the .env file")
+	}
+
+	//define a client for loggly
+	client := loggly.New("nazartrut")
+
+	//see if the path is correct, if not throw a 400 bad request error
+	fmt.Println(r.URL.Path)
+	if r.URL.Path != "/ntrut/all" {
+		client.Send("error", "Error, bad request for the endpoint /ntrut/all")
+		//send back 400 status code
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode("400 - Bad Request")
+	}
+
+	// Create an AWS session for US East 1. REE
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	}))
+
+	// Create a DynamoDB instance
+	db := dynamodb.New(sess)
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method != http.MethodGet {
+		client.Send("error", "Incorrect Method, Needs to be GET. Endpoint: /ntrut/all")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		json.NewEncoder(w).Encode("405 - Method Not Allowed")
+	} else {
+		//query the db
+		params := &dynamodb.ScanInput{
+			TableName: aws.String("Crypto"),
+		}
+
+		//scan the db, this returns all items
+		result, err := db.Scan(params)
+		if err != nil {
+			client.Send("error", "Error getting all items from Crpto Table. Endpoint: /ntrut/all")
+		} else {
+			//create an array, then unshall the gotten items from the scan
+			var info []Item
+			err = dynamodbattribute.UnmarshalListOfMaps(result.Items, &info)
+			if err != nil {
+				_ = client.Send("error", "Got error unmarshalling items. Endpoint: /ntrut/all")
+			}
+
+			//success
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(info)
+			client.Send("info", "Successfully returned all items from Cryto dynamoDB table. Length of data: "+strconv.Itoa(len(info)))
+		}
+	}
+}
+
 func main() {
 	//define a client for logglyy
 	r := mux.NewRouter()
 	r.HandleFunc("/ntrut/status", statusHandler).Methods("GET")
+	r.HandleFunc("/ntrut/all", all).Methods("GET")
 	http.ListenAndServe(":8080", r)
 
 }
